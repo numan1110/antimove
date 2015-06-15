18ffccba55df607c8be2cb231d80a9d4d499209f
fullinterest <miku35zatsune-web@yahoo.fr>
2015-06-15 22:31:40 +0200

    .

--- settings.lua
+++ settings.lua
@@ -1,5 +1,5 @@
 -- Whitelist of unique identifiers, of people allowed to move you (so no move back to the last channel)
-antimove_whitelist = {} 
+antimove_whitelist = {}
 
 -- Stores if the script should be working by default when loaded (true by default, so, working)
 antimove_enabled = true
